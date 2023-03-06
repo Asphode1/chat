aa99f692ad71caaaf2f36c12ecb1a419391bcbfb
Asphode1 <maitrungkien1910@gmail.com>
2023-03-06 19:24:11 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	css: {
-		postcss: {
-			plugins: [autoprefixer({}), tailwindcss({})],
-		},
-	},
+  plugins: [react()],
+  base: './',
+  css: {
+    postcss: {
+      plugins: [autoprefixer({}), tailwindcss({})],
+    },
+  },
 })
