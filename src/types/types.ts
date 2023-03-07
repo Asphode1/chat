73f9ca5d1ccd629f34a61857122df1138a12a44c
Asphode1 <maitrungkien1910@gmail.com>
2023-03-07 19:31:28 +0700

    update

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,40 +1,46 @@
-export type Json = string | number | boolean | null | { [key: string]: Json } | Json[]
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
 
 export interface Database {
-	public: {
-		Tables: {
-			chat: {
-				Row: {
-					chatmessage: string
-					created_at: string
-					room: number
-					userid: number
-				}
-				Insert: {
-					chatmessage: string
-					created_at?: string
-					room: number
-					userid?: number
-				}
-				Update: {
-					chatmessage?: string
-					created_at?: string
-					room?: number
-					userid?: number
-				}
-			}
-		}
-		Views: {
-			[_ in never]: never
-		}
-		Functions: {
-			[_ in never]: never
-		}
-		Enums: {
-			[_ in never]: never
-		}
-		CompositeTypes: {
-			[_ in never]: never
-		}
-	}
+  public: {
+    Tables: {
+      chat: {
+        Row: {
+          chatmessage: string
+          created_at: string
+          room: number
+          userid: number
+        }
+        Insert: {
+          chatmessage: string
+          created_at?: string
+          room: number
+          userid?: number
+        }
+        Update: {
+          chatmessage?: string
+          created_at?: string
+          room?: number
+          userid?: number
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
 }
