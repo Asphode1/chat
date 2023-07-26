d8bf835101154ab669443142c5ab445669c9e722
Asphodel <maitrungkien1910@gmai.com>
2023-07-26 23:16:07 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import TabProvider from './context/tab-context'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
